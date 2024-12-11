1dc45eb2ac0e21e2696ab93b4f3d175ab583b764
alina-gasperino <alinacreatesthings@gmail.com>
2024-12-11 03:39:18 -0500

    layout

---
+++ templates/layout/breadcrumb.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Part: Breadcrumb
+ *
+ * @since 1.4.4
+ */
+
+?>
+
+<?php if( function_exists( 'get_breadcrumb' ) ): ?>
+
+	<div class="site-breadcrumb">
+
+		<?php get_breadcrumb(); ?>
+		
+	</div>
+
+<?php endif; ?>
